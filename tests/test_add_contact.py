4b7d2067573d7596d304c83df209d888556592ce
YatsenkoRI <romanrcen@gmail.com>
2019-02-23 15:22:51 +0700

    Выделены классы для работы с группами и контактами.

--- tests/test_add_contact.py
+++ tests/test_add_contact.py
@@ -16,6 +16,7 @@ def test_add_contact(app):
     app.contact.create(Contact(first_name='Roman',
                                last_name='Yatsenko',
                                company='ModulKassa',
+                               address='Russia, Novosibirsk, Planetnaya st., 30',
                                mobile_phone='+79529316917',
                                email='test@test.ru',
                                notes='This is just a text for test of notes.'))
