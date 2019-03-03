f5fdb6f25b65d96b2248006c6e2e64f7ed7b1e38
YatsenkoRI <romanrcen@gmail.com>
2019-03-03 22:52:27 +0700

    Оптимизированы переходы между страницами.

--- fixture/contact.py
+++ fixture/contact.py
@@ -7,11 +7,13 @@ class ContactHelper:
 
     def open_contact_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("add new").click()
+        if not (wd.current_url.endswith('/edit.php') and len(wd.find_elements_by_name('firstname')) > 0):
+            wd.find_element_by_link_text("add new").click()
 
     def open_home_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("home").click()
+        if not (wd.current_url.endswith('/addressbook/') and len(wd.find_elements_by_name('searchstring')) > 0):
+            wd.find_element_by_link_text("home").click()
 
     def change_field_value(self, field_name, text):
         wd = self.app.wd
